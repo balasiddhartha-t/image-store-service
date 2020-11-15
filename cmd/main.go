01000764dd25d084e215209da2c88241eccd9a9b
Siddhartha <balasiddhartha@live.com>
2020-11-16 04:05:22 +0530

    Changing the port number

--- cmd/main.go
+++ cmd/main.go
@@ -14,16 +14,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	handler "image-store-service/pkg/handlers"
 
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	r := mux.NewRouter()
+	log.Println("Server has been started")
 
 	// handlers for API
 	r.Handle("/createAlbum", handler.CreateAlbumHandler()).Methods("POST")
@@ -33,13 +36,13 @@ func main() {
 	r.Handle("/getImage", handler.GetImageHandler()).Methods("GET")
 
 	r.Handle("/swagger.yaml", http.FileServer(http.Dir("../"))).Methods("GET")
-
+	http.Handle("/metrics", promhttp.Handler())
 	// handler for documentation
 	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
 	sh := middleware.Redoc(opts, nil)
 
 	r.Handle("/docs", sh).Methods("GET")
 
-	http.ListenAndServe(":8081", r)
+	http.ListenAndServe(":8080", r)
 
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,9 @@ go 1.13
 
 require (
 	github.com/Shopify/sarama v1.27.0
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/go-openapi/runtime v0.19.21
 	github.com/gorilla/mux v1.8.0
+	github.com/prometheus/client_golang v1.7.1
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
